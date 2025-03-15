31f1cdb3a0dab49567c76af4073b6dd9f70b5dff
oxce5_root <avg.gamer@proton.me>
2025-03-15 22:05:04 +0800

    update: font

--- home-manager/ghostty.nix
+++ home-manager/ghostty.nix
@@ -5,6 +5,7 @@
     enableZshIntegration = true;
     settings = {
       background-opacity = 0.6;  
+      font-family = "JetBrainsMono Nerd Font Mono";
       theme = "tokyonight-storm";
       custom-shader = "~/.config/ghostty/bloom.glsl";
     };
