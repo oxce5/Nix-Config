e147ebcb75bcdc7702e9b578cf4800836acf4d4e
oxce5_root <avg.gamer@proton.me>
2025-03-25 11:37:08 +0800

    add: waydroid firewall rules

--- nixos/networking.nix
+++ nixos/networking.nix
@@ -3,6 +3,9 @@
 {
   networking.networkmanager.enable = true;  
   networking.firewall.allowedTCPPorts = [
+    67
+    53
+    68
     80   # HTTP
     82   # Custom Port (adjust as necessary)
     443  # HTTPS
@@ -10,11 +13,18 @@
     8080
   ];
   networking.firewall.allowedUDPPorts = [
+    67
+    53
+    68
     80
     82
     443
     3301
   ];
   networking.firewall.enable = true;
-}
+  networking.firewall.extraForwardRules = [
+    "-j ACCEPT"
+  ];
+
+  }
 
