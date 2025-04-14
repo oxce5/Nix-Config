546cc7000451378933f2d8cebb9253eee89ecdcf
oxce5 <avg.gamer@proton.me>
2025-04-14 22:14:11 +0800

    117 current  2025-04-14 22:13:50  25.05.20250403.2bfc080  6.14.0-zen1                          *

--- modules/system/default.nix
+++ modules/system/default.nix
@@ -77,6 +77,13 @@
     waydroid.enable = true;
   };
 
+  networking = {
+    firewall = {
+      enable = true;
+      allowedUDPPorts = [19132 19133];
+    };
+  };
+
   systemd.packages = [pkgs.cloudflare-warp]; # for warp-cli
   systemd.targets.multi-user.wants = ["warp-svc.service"]; # causes warp-svc to be started automatically
   zramSwap = {
