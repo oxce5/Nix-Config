2643199df49aa1f43328621ceb1a21b7b67744f6
oxce5 <avg.gamer@proton.me>
2025-04-14 14:49:35 +0800

    107 current  2025-04-14 14:49:15  25.05.20250403.2bfc080  6.14.0-zen1                          *

--- pkgs/crafty.nix
+++ pkgs/crafty.nix
@@ -15,30 +15,34 @@ stdenv.mkDerivation rec {
     sha256 = "ea59f0d5eceb86f790e54a826d1e947e970d3f9d"; # Replace with the actual sha256
   };
 
-  buildInputs = [python3];
+  buildinputs = [python3];
 
-  # Build steps (just install dependencies and set up the venv)
-  installPhase = ''
-    mkdir -p $out/lib/crafty
-    cp -r . $out/lib/crafty/crafty-4
-    cd $out/lib/crafty/crafty-4
+  # build steps
+  unpackphase = ''
+    echo "unpacking the repository"
+    cp -r ${src} $out
+  '';
+
+  installphase = ''
+    echo "installing crafty..."
+    cd $out
 
     python3 -m venv .venv
     source .venv/bin/activate
-    pip install --no-cache-dir -r requirements.txt
+    pip install --no-cache-dir -r crafty-4/requirements.txt
 
     mkdir -p $out/bin
-    cat > $out/bin/crafty <<EOF
+    cat > $out/bin/crafty <<eof
     #!${stdenv.bash}/bin/bash
-    cd $out/lib/crafty/crafty-4
+    cd $out/crafty-4
     source .venv/bin/activate
-    exec python main.py "\$@"
-    EOF
+    exec python3 main.py "\$@"
+    eof
     chmod +x $out/bin/crafty
   '';
 
   meta = with lib; {
-    description = "Crafty Minecraft Controller";
+    description = "crafty minecraft controller";
     license = licenses.mit;
     platforms = platforms.linux;
   };
