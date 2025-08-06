fc8dde4d22c6202d9da751fafe5624a3701eb831
oxce5 <avg.gamer@proton.me>
2025-08-07 01:00:20 +0800

    change: default cursor is now Teto_Cursor

---
+++ modules/dots/default.nix
@@ -0,0 +1,31 @@
+{
+  inputs,
+  pkgs,
+  lib,
+  ...
+}: {
+  imports = [
+    ./ghostty
+    ./hyde
+    ./hypr
+    ./mangohud
+    ./nvim
+    # ./ricecursor
+    ./tetocur
+    ./yazi
+    ./zsh
+  ];
+
+  home.packages = with pkgs; [
+    # (import ./kurukuru {
+    #   inherit symlinkJoin makeWrapper runCommandLocal quickshell kdePackages lib;
+    #   makeFontsConf = pkgs.makeFontsConf;
+    #   nerd-fonts = pkgs.nerd-fonts.caskaydia-mono;
+    #   material-symbols = pkgs.material-symbols;
+    #   configPath = ./kurukuru/kurukurubar;
+    #   asGreeter = false;
+    #   customColors = null;
+    # })
+    inputs.zaphkiel.packages.${pkgs.system}.kurukurubar-unstable
+  ];
+}
