0c8029b17d2121a840c8875656062e0462acfb51
oxce5 <avg.gamer@proton.me>
2025-08-07 01:03:50 +0800

    change: aagl to false, this just wasted space and electrons

--- modules/system/aagl.nix
+++ modules/system/aagl.nix
@@ -2,7 +2,7 @@
   imports = [inputs.aagl.nixosModules.default];
   nix.settings = inputs.aagl.nixConfig; # Set up Cachix
   aagl.enableNixpkgsReleaseBranchCheck = false;
-  programs.anime-game-launcher.enable = true; # Adds launcher and /etc/hosts rules
+  programs.anime-game-launcher.enable = false; # Adds launcher and /etc/hosts rules
   programs.anime-games-launcher.enable = false;
   programs.honkers-railway-launcher.enable = true;
   programs.honkers-launcher.enable = false;
