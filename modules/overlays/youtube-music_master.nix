8a621297043578cb12918ea5f8dd534e5073b6b3
oxce5 <avg.gamer@proton.me>
2025-08-17 13:34:58 +0800

    update hash

--- modules/overlays/youtube-music_master.nix
+++ modules/overlays/youtube-music_master.nix
@@ -5,14 +5,14 @@ self: super: {
       owner = "th-ch";
       repo = "youtube-music";
       rev = "master";
-      hash = "sha256-fzS4dLA0NAbBklbgITcqVA/2MLfXHCI2au6OmTzIk+Q="; 
+      hash = "sha256-fzS4dLA0NAbBklbgITcqVA/2MLfXHCI2au6OmTzIk+Q=";
     };
     pnpmDeps = super.pnpm.fetchDeps {
       pname = "youtube-music";
       version = "master";
       src = src;
       fetcherVersion = 1;
-      hash = "sha256-MBiYTKef9bCHbLmFUWXKIc9YwnYIngjaXv0rXlp8dQU="; 
+      hash = "sha256-MBiYTKef9bCHbLmFUWXKIc9YwnYIngjaXv0rXlp8dQU=";
     };
   });
 }
