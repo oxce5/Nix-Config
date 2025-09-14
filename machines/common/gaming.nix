edc7bdb9678bbfe1d08583397c28d4519c03fa0a
oxce5 <avg.gamer@proton.me>
2025-09-14 09:55:19 +0800

    move: steam to gaming.nix

--- machines/common/gaming.nix
+++ machines/common/gaming.nix
@@ -1,7 +1,35 @@
 {pkgs, ...}: {
-  programs.steam = {
-    enable = true;
-    remotePlay.openFirewall = true;
-    dedicatedServer.openFirewall = true;
+  programs = {
+    steam = {
+      enable = true;
+
+      remotePlay.openFirewall = true;
+      dedicatedServer.openFirewall = true;
+    };
+    # Fixes using gamescope in steam
+    steam.package = pkgs.steam.override {
+      extraPkgs = pkgs:
+        with pkgs; [
+          xorg.libXcursor
+          xorg.libXi
+          xorg.libXinerama
+          xorg.libXScrnSaver
+          libpng
+          libpulseaudio
+          libvorbis
+          stdenv.cc.cc.lib
+          libkrb5
+          keyutils
+        ];
+    };
+
+    gamescope.enable = true;
+    gamescope.capSysNice = false; # Breaks gamescope in steam
+
+    nix-ld = {
+      enable = true;
+      libraries = pkgs.steam-run.args.multiPkgs pkgs;
+    };
+    gamemode.enable = true;
   };
 }
