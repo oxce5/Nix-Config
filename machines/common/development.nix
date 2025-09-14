fb00547e8b1b2fbab7b98b94934c5fa092844869
oxce5 <avg.gamer@proton.me>
2025-09-14 09:55:08 +0800

    move: docker to development.nix

--- machines/common/development.nix
+++ machines/common/development.nix
@@ -1,11 +1,21 @@
 {pkgs, ...}: {
   environment.systemPackages = with pkgs; [
     devenv
+    distrobox
   ];
 
   programs.direnv = {
     enable = true;
     enableZshIntegration = true;
     loadInNixShell = true;
+  };
+
+  virtualisation.containers.enable = true;
+  virtualisation.docker = {
+    enable = true;
+    rootless = {
+      enable = true;
+      setSocketVariable = true;
+    };
   };
 }
