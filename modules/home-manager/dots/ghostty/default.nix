dc21aafe0a7c7652fa7172ae47f21fe891c75c4f
oxce5 <avg.gamer@proton.me>
2025-09-27 19:16:41 +0800

    add theme, vim integration and fish

--- modules/home-manager/dots/ghostty/default.nix
+++ modules/home-manager/dots/ghostty/default.nix
@@ -6,14 +6,19 @@
   programs.ghostty = {
     enable = true;
     package = inputs.ghostty.packages.${pkgs.system}.default;
+    enableBashIntegration = true;
     enableZshIntegration = true;
+    enableFishIntegration = true;
     installVimSyntax = true;
     settings = {
+      theme = "Tomorrow Night Burns"; 
       custom-shader = builtins.toString ./manga_slash.glsl;
       custom-shader-animation = true;
 
       font-family = "JetBrainsMono Nerd Font";
       font-size = 10;
+
+      adjust-underline-position = 3;
 
       gtk-titlebar = false;
 
@@ -22,6 +27,10 @@
 
       unfocused-split-opacity = 1;
       focus-follows-mouse = true;
+
+      
+      window-padding-x = 10;
+      window-padding-y = 10;
 
       keybind = ["ctrl+shift+d=close_surface"];
     };
