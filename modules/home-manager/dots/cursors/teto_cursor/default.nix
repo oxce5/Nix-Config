c0936e38c94f8f20effe98bf62ccaff527847be7
oxce5 <avg.gamer@proton.me>
2025-10-19 16:05:59 +0800

    archive tetocursor

--- modules/home-manager/dots/cursors/teto_cursor/default.nix
+++ modules/home-manager/dots/cursors/teto_cursor/default.nix
@@ -2,10 +2,10 @@
 stdenv.mkDerivation {
   pname = "teto-cursor";
   version = "1.0";
-  src = ./TetoCur.tar.xz;
+  src = ./TetoCursor.tar.xz;
 
   installPhase = ''
-    mkdir -p $out/share/icons/Teto_Cursor
-    tar -xf $src -C $out/share/icons/Teto_Cursor
+    mkdir -p $out/share/icons/TetoCursor
+    tar --strip-components=1 -xf $src -C $out/share/icons/TetoCursor
   '';
 }
