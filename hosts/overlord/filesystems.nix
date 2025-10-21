7e81313a45251d42d8b2b01bd443a855bb499eb8
oxce5 <avg.gamer@proton.me>
2025-10-21 08:11:38 +0800

    formatting

--- hosts/overlord/filesystems.nix
+++ hosts/overlord/filesystems.nix
@@ -1,6 +1,6 @@
 {
   unify.hosts.nixos.overlord = {
-    nixos = {
+    nixos = {lib, ...}: {
       fileSystems."/" = {
         device = "/dev/disk/by-uuid/5768cdb0-f45a-46a6-9802-738595c7e079";
         fsType = "ext4";
@@ -8,7 +8,7 @@
       fileSystems."/boot" = {
         device = "/dev/disk/by-uuid/1A3A-626E";
         fsType = "vfat";
-        options = [ "fmask=0077" "dmask=0077" ];
+        options = ["fmask=0077" "dmask=0077"];
       };
     };
   };
