c710cb0d9895119a02f07dd7cbc5743728126f6b
avg.gamer <avg.gamer@proton.me>
2025-10-30 08:50:51 +0800

    fix pihole

--- modules/server/pihole.nix
+++ modules/server/pihole.nix
@@ -1,16 +1,55 @@
 {
   unify.modules.server = {
     nixos = {
-      services = {
-        pihole-ftl = {
-          enable = true;
-        };
+      pkgs,
+      hostConfig,
+      ...
+    }: {
+      networking.firewall.allowedTCPPorts = [6060];
+      users.users.${hostConfig.primaryUser}.extraGroups = [
+        "pihole"
+      ];
 
-        pihole-web = {
-          enable = true;
-          ports =[ "80r" "443s" ];
+      services.pihole-ftl = {
+        enable = true;
+        openFirewallDNS = true;
+        openFirewallDHCP = true;
+        queryLogDeleter.enable = true;
+        lists = [
+          {
+            url = "https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts";
+            # Alternatively, use the file from nixpkgs. Note its contents won't be
+            # automatically updated by Pi-hole, as it would with an online URL.
+            # url = "file://${pkgs.stevenblack-blocklist}/hosts";
+            description = "Steven Black's unified adlist";
+          }
+        ];
+        settings = {
+          dns = {
+            domainNeeded = true;
+            expandHosts = true;
+            interface = "enp0s31f6";
+            listeningMode = "BIND";
+            upstreams = "8.8.8.8";
+          };
+          dhcp = {
+            active = false;
+          };
         };
       };
+
+      # Pi-hole web interface
+      services.pihole-web = {
+        enable = true;
+        ports = [6060];
+      };
+
+      # Pi-hole related packages
+      environment.systemPackages = with pkgs; [
+        pihole-ftl
+        pihole-web
+        pihole
+      ];
     };
 
     home = {
