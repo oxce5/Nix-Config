92acffa6374570488a97b8f6d0f682225287a402
avg.gamer <avg.gamer@proton.me>
2025-10-30 08:50:45 +0800

    add cockpit for dashboard

--- modules/server/default.nix
+++ modules/server/default.nix
@@ -1,6 +1,26 @@
 {
   unify.modules.server = {
-    nixos = {
+    nixos = {pkgs, ...}: {
+      environment.systemPackages = with pkgs; [
+        cockpit
+      ];
+
+      services.cockpit = {
+        enable = true;
+        port = 9090;
+        openFirewall = true; # Please see the comments section
+        settings = {
+          # WebService = {
+          #   AllowUnencrypted = true;
+          # };
+        };
+        allowed-origins = [
+          "https://192.168.1.26:9090"
+          "ws://192.168.1.26:9090"
+          "https://chimera:9090"
+          "ws://chimera:9090"
+        ];
+      };
     };
     home = {
     };
