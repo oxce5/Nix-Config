7bbac9939c007c7644910611d27cbb4eae42a9d3
avg.gamer <avg.gamer@proton.me>
2025-10-30 08:50:21 +0800

    configure searxng

--- modules/server/searxng.nix
+++ modules/server/searxng.nix
@@ -1,11 +1,18 @@
 {
   unify.modules.server = {
     nixos = {
+      networking.firewall.allowedTCPPorts = [8080];
+
       services = {
         searx = {
           enable = true;
           configureNginx = true;
           domain = "searxng.nix";
+          settings = {
+            server.port = 8080;
+            server.bind_address = "0.0.0.0";
+            server.secret_key = "lalalala";
+          };
         };
       };
     };
