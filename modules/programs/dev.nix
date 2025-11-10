e8b9fb4897dfb77ee01f3b2a45a4ad7ad46b47c6
avg.gamer <avg.gamer@proton.me>
2025-11-10 09:15:54 +0800

    add docker to dev.nix

--- modules/programs/dev.nix
+++ modules/programs/dev.nix
@@ -1,13 +1,25 @@
 {
-  unify.modules.workstation.home = {pkgs, ...}: {
-    home.packages = with pkgs; [
-      # Dev
-      lazyjj
-      lazygit
-      jq
+  unify.modules.workstation = {
+    nixos = {
+      pkgs,
+      hostConfig,
+      ...
+    }: {
+      virtualisation.docker.enable = true;
+      users.users.${hostConfig.primaryUser}.extraGroups = ["docker"];
+    };
+    home = {pkgs, ...}: {
+      home.packages = with pkgs; [
+        # Dev
+        lazyjj
+        lazygit
+        jq
 
-      devenv
-      uv
-    ];
+        devenv
+        uv
+
+        just
+      ];
+    };
   };
 }
