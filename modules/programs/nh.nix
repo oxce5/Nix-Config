b414b616cfdd3f77d012291eed706100451dac07
avg.gamer <avg.gamer@proton.me>
2025-11-10 09:15:06 +0800

    use nix.gc

--- modules/programs/nh.nix
+++ modules/programs/nh.nix
@@ -9,7 +9,7 @@
       programs.nh = {
         enable = true;
         package = pkgs.nh;
-        clean.enable = true;
+        clean.enable = false;
         clean.extraArgs = "--keep-since 4d --keep 5";
         flake = "/home/${hostConfig.primaryUser}/.flake";
       };
