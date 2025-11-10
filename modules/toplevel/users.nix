63918ba8380c4c4a03a30dc0e4bc2953afd930b9
avg.gamer <avg.gamer@proton.me>
2025-11-10 09:16:42 +0800

    separate each user

--- modules/toplevel/users.nix
+++ modules/toplevel/users.nix
@@ -1,28 +1,36 @@
 {
   unify = {
+    nixos = {
+      pkgs,
+      hostConfig,
+      ...
+    }: {
+      users.users.${hostConfig.primaryUser} = {
+        shell = pkgs.fish;
+        extraGroups = [
+          "wheel"
+          "video"
+          "audio"
+          "networkmanager"
+        ];
+      };
+    };
     modules.server.nixos = {hostConfig, ...}: {
       users.users.${hostConfig.primaryUser} = {
+        isNormalUser = true;
         linger = true;
         openssh.authorizedKeys.keys = [
           "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIkV1+Pn73xbBHobsgg2DYgaE8T0e0ngpew50x3Silsg avg.gamer@proton.me"
         ];
       };
     };
-    nixos = {
-      hostConfig,
-      pkgs,
-      ...
-    }: {
+    modules.laptop.nixos = {hostConfig, ...}: {
       users = {
         groups.${hostConfig.primaryUser} = {};
         users.${hostConfig.primaryUser} = {
           isNormalUser = true;
           initialPassword = hostConfig.primaryUser;
           extraGroups = [
-            "wheel"
-            "video"
-            "audio"
-            "networkmanager"
             "lp"
             "scanner"
             "docker"
@@ -33,7 +41,6 @@
             "aria2"
             "keyd"
           ];
-          shell = pkgs.fish;
         };
       };
     };
