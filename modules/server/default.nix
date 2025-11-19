82f54162ab0bfe4dcdbb87db0a3ea183eae26c6b
avg.gamer <avg.gamer@proton.me>
2025-11-19 09:45:36 +0800

    TODO: implement dashboard

--- modules/server/default.nix
+++ modules/server/default.nix
@@ -1,28 +1,12 @@
 {
   unify.modules.server = {
     nixos = {pkgs, ...}: {
-      environment.systemPackages = with pkgs; [
-        cockpit
-      ];
-
-      services.cockpit = {
-        enable = true;
-        port = 9090;
-        openFirewall = true; # Please see the comments section
-        settings = {
-          # WebService = {
-          #   AllowUnencrypted = true;
-          # };
-        };
-        allowed-origins = [
-          "https://192.168.1.26:9090"
-          "ws://192.168.1.26:9090"
-          "https://chimera:9090"
-          "ws://chimera:9090"
-        ];
-      };
     };
-    home = {
+    home = {pkgs, ...}: {
+      home.packages = with pkgs; [
+        neovim
+        distant
+      ];
     };
   };
 }
