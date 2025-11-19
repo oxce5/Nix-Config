6a2df74dfa16f00984a00108a18d80cef6615b76
avg.gamer <avg.gamer@proton.me>
2025-11-19 09:45:09 +0800

    update PS1 prompt hash]

--- modules/programs/shell.nix
+++ modules/programs/shell.nix
@@ -11,7 +11,7 @@
 
     nerd-fonts-prompt = builtins.fromTOML (builtins.readFile (pkgs.fetchurl {
       url = "https://starship.rs/presets/toml/nerd-font-symbols.toml";
-      sha256 = "1wrzcsfgf0r9fsy8w144jhm5y6zrbafsx5qcldsfks1y105jxl1j";
+      sha256 = "sha256-KssAxwGVYGGBMcH6Lwq1sllYwqrD95TfpcjydMtCceM=";
     }));
 
     enableIntegrations = shells:
