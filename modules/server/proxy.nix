c667fa601f26f8559a8b315dfedb3be6d655b0ed
avg.gamer <avg.gamer@proton.me>
2025-11-30 20:20:34 +0800

    replace npm with traefik

--- modules/server/proxy.nix
+++ modules/server/proxy.nix
@@ -1,28 +1,128 @@
 {
-  unify.modules.proxy.nixos = {
-    pkgs,
-    hostConfig,
-    ...
-  }: {
+  unify.modules.proxy.nixos = {config, ...}: {
     networking.firewall.allowedTCPPorts = [
       81
     ];
+    services = {
+      tailscale.permitCertUid = "traefik";
 
-    virtualisation.oci-containers.containers.nginx-proxy-manager = {
-      image = "jc21/nginx-proxy-manager:latest";
-      autoStart = true;
-      extraOptions = [
-        "--network=host"
-      ];
-      volumes = [
-        "/home/${hostConfig.primaryUser}/Services/nginx-proxy-manager/data:/data"
-        "/home/${hostConfig.primaryUser}/Services/nginx-proxy-manager/lets_encrypt:/etc/letsencrypt"
-      ];
-      ports = [
-        "80:80"
-        "443:443"
-        "81:81"
-      ];
+      traefik = {
+        enable = true;
+        staticConfigOptions = {
+          entryPoints = {
+            web = {
+              address = ":80";
+              asDefault = true;
+              http.redirections.entrypoint = {
+                to = "websecure";
+                scheme = "https";
+              };
+            };
+
+            websecure = {
+              address = ":443";
+              asDefault = true;
+              http.tls.certResolver = "tailscale";
+            };
+          };
+          certificatesResolvers = {
+            tailscale.tailscale = {};
+          };
+          log = {
+            level = "INFO";
+            filePath = "${config.services.traefik.dataDir}/traefik.log";
+            format = "json";
+          };
+          api = {
+            dashboard = true;
+            insecure = true;
+          };
+        };
+        dynamicConfigOptions = {
+          http = {
+            routers = {
+              pihole = {
+                entryPoints = ["websecure"];
+                rule = "Host(`pihole.tilapia-morpho.ts.net`)";
+                service = "pihole";
+                middlewares = ["authelia-auth" ];
+              };
+
+              n8n = {
+                entryPoints = ["websecure"];
+                rule = "Host(`n8n.tilapia-morpho.ts.net`)";
+                service = "n8n";
+                middlewares = ["authelia-auth"];
+              };
+
+              sillytavern = {
+                entryPoints = ["websecure"];
+                rule = "Host(`sillytavern.tilapia-morpho.ts.net`)";
+                service = "sillytavern";
+                middlewares = ["authelia-auth"];
+              };
+
+              auth = {
+                entryPoints = ["websecure"];
+                rule = "Host(`auth.tilapia-morpho.ts.net`)";
+                service = "auth";
+              };
+            };
+
+            services = {
+              auth = {
+                loadBalancer.servers = [{ url = "http://127.0.0.1:9091"; }];
+              };
+
+              pihole = {
+                loadBalancer = {
+                  servers = [{ url = "http://127.0.0.1:6060"; }];
+                };
+              };
+
+              n8n = {
+                loadBalancer = {
+                  servers = [{ url = "http://127.0.0.1:5678"; }];
+                };
+              };
+
+              sillytavern = {
+                loadBalancer = {
+                  servers = [{ url = "http://127.0.0.1:8000"; }];
+                };
+              };
+            };
+
+            middlewares = {
+              strip-pihole = {
+                stripPrefix = {
+                  prefixes = ["/pihole"];
+                };
+              };
+
+              strip-n8n = {
+                stripPrefix = {
+                  prefixes = ["/n8n"];
+                };
+              };
+
+              authelia-auth = {
+                forwardAuth = {
+                  address = "http://127.0.0.1:9091/api/verify?rd=https://auth.tilapia-morpho.ts.net";
+                  trustForwardHeader = true;
+                  authResponseHeaders = [
+                    "Remote-User"
+                    "Remote-Groups"
+                    "Remote-Name"
+                    "Remote-Email"
+                    "Remote-Preferred-Username"
+                  ];
+                };
+              };
+            };
+          };
+        };
+      };
     };
   };
 }
