73c418977b82c02508c8e638ef61c10548e45e84
avg.gamer <avg.gamer@proton.me>
2025-11-30 20:25:24 +0800

    add hacking machine

--- hosts/rei/default.nix
+++ hosts/rei/default.nix
@@ -5,25 +5,31 @@
 }: {
   unify.hosts.nixos.rei = {
     modules = with config.unify.modules; [
-      bar
       hacking
       hyprland
       ssh
+      zellij
+      stylix
+      ghostty
     ];
     # primaryDisplay = config.unify.hosts.nixos.rei.displays.eDP-1;
     primaryUser = "adachi";
     primaryWallpaper = builtins.fetchurl {
-      url = "https://w.wallhaven.cc/full/21/wallhaven-2119vy.jpg";
-      sha256 = "0l67mxk5hz2vx1nz7bmk5pzszmw3pafzlw5m94x8b7yp6qnc6fmk";
+      url = "https://static.zerochan.net/Adachi.Rei.full.3685990.jpg";
+      sha256 = "sha256-grmog1BGo1x1rjNoUXQMNlWXdfi/ZxZ7cASZIZt9R4k=";
     };
     baseImage = builtins.fetchurl {
-      url = "https://w.wallhaven.cc/full/21/wallhaven-2119vy.jpg";
-      sha256 = "0l67mxk5hz2vx1nz7bmk5pzszmw3pafzlw5m94x8b7yp6qnc6fmk";
+      url = "https://s3.zerochan.net/Adachi.Rei.240.3683316.jpg";
+      sha256 = "sha256-oxc+99D+3i2yPq1vq7UdI1kNC87V4CDhwKRNc1SbLEs=";
     };
 
     users.adachi.modules = config.unify.hosts.nixos.rei.modules;
 
-    nixos = {pkgs, ...}: {
+    nixos = {
+      pkgs,
+      hostConfig,
+      ...
+    }: {
       facter.reportPath = ./facter.json;
       imports = with inputs; [
         nixos-hardware.nixosModules.common-cpu-amd
@@ -39,8 +45,13 @@
 
       networking = {
         networkmanager.enable = true;
+        firewall.enable = false;
         hostName = "rei";
       };
+
+      environment.systemPackages = [pkgs.uv pkgs.neovim pkgs.openvpn pkgs.brave];
+
+      environment.pathsToLink = ["/share/applications" "/share/xdg-desktop-portal"];
     };
   };
 }
