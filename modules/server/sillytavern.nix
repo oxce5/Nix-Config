d467616fd807a19352225d7e600d102dc94d8120
avg.gamer <avg.gamer@proton.me>
2025-12-07 12:00:29 +0800

    move to services.podman (will be migrating to services.sillytavern)

--- modules/server/sillytavern.nix
+++ modules/server/sillytavern.nix
@@ -1,30 +1,30 @@
 {
-  unify.modules.sillytavern.home = {
-    pkgs,
-    lib,
-    ...
-  }: {
-    services.podman.containers.sillytavern = {
-      image = "ghcr.io/sillytavern/sillytavern:latest";
-      user = "sillytavern";
-      environment = {
-        "FORCE_COLOR" = "1";
-        "NODE_ENV" = "production";
+  unify.modules.sillytavern = {
+    home = {
+      services.podman = {
+        containers.sillytavern = {
+          image = "ghcr.io/sillytavern/sillytavern:latest";
+          environment = {
+            "FORCE_COLOR" = "1";
+            "NODE_ENV" = "production";
+          };
+          network = "host";
+          volumes = [
+            "/home/sillytavern/SillyTavern/config:/home/node/app/config:rw,Z"
+            "/home/sillytavern/SillyTavern/data:/home/node/app/data:rw,Z"
+            "/home/sillytavern/SillyTavern/extensions:/home/node/app/public/scripts/extensions/third-party:rw,Z"
+            "/home/sillytavern/SillyTavern/plugins:/home/node/app/plugins:rw,Z"
+            "/home/sillytavern/SillyTavern/backups:/home/node/app/backups:rw,Z"
+          ];
+          ports = [
+            "8000:8000"
+          ];
+          extraPodmanArgs = [
+            "--hostname=sillytavern"
+          ];
+          autoStart = true;
+        };
       };
-      volumes = [
-        "/home/sillytavern/SillyTavern/config:/home/node/app/config:rw,Z"
-        "/home/sillytavern/SillyTavern/data:/home/node/app/data:rw,Z"
-        "/home/sillytavern/SillyTavern/extensions:/home/node/app/public/scripts/extensions/third-party:rw,Z"
-        "/home/sillytavern/SillyTavern/plugins:/home/node/app/plugins:rw,Z"
-      ];
-      ports = [
-        "8000:8000/tcp"
-      ];
-      extraOptions = [
-        "--hostname=sillytavern"
-      ];
-      autoStart = true;
-      restartPolicy = "always";
     };
   };
 }
