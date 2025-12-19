b81dbac5f9ce114dedc2ac8216a68c7f24e6fea5
avg.gamer <avg.gamer@proton.me>
2025-12-19 14:41:09 +0800

    idk why this still exists

--- modules/server/monitoring.nix
+++ modules/server/monitoring.nix
@@ -11,27 +11,5 @@
         PORT = "40000";
       };
     };
-    services.podman.containers.sillytavern = {
-      image = "portainer/portainer-ce:lts";
-      environment = {
-        "FORCE_COLOR" = "1";
-        "NODE_ENV" = "production";
-      };
-      network = "host";
-      volumes = [
-        "/home/sillytavern/SillyTavern/config:/home/node/app/config:rw,Z"
-        "/home/sillytavern/SillyTavern/data:/home/node/app/data:rw,Z"
-        "/home/sillytavern/SillyTavern/extensions:/home/node/app/public/scripts/extensions/third-party:rw,Z"
-        "/home/sillytavern/SillyTavern/plugins:/home/node/app/plugins:rw,Z"
-        "/home/sillytavern/SillyTavern/backups:/home/node/app/backups:rw,Z"
-      ];
-      ports = [
-        "8000:8000"
-      ];
-      extraPodmanArgs = [
-        "--hostname=sillytavern"
-      ];
-      autoStart = true;
-    };
   };
 }
