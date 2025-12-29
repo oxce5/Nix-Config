84afd04047b5cd10e9d65fb9e6293474208c8f4b
avg.gamer <avg.gamer@proton.me>
2025-12-29 23:38:34 +0800

    add dms greeter

--- modules/programs/dms.nix
+++ modules/programs/dms.nix
@@ -1,28 +1,44 @@
 {inputs, ...}: {
-  unify.modules.dms.home = {pkgs, ...}: {
-    imports = [
-      inputs.dms.homeModules.dankMaterialShell.default
-      inputs.dms.homeModules.dankMaterialShell.niri
-    ];
+  unify.modules.dms = {
+    home = {pkgs, ...}: {
+      imports = [
+        inputs.dms.homeModules.dankMaterialShell.default
+        inputs.dms.homeModules.dankMaterialShell.niri
+      ];
 
-    programs.dankMaterialShell = {
-      enable = true;
-
-      systemd = {
+      programs.dankMaterialShell = {
         enable = true;
-        restartIfChanged = true;
+
+        systemd = {
+          enable = true;
+          restartIfChanged = true;
+        };
+
+        enableSystemMonitoring = true;
+        enableClipboard = true;
+        enableVPN = true;
+        enableDynamicTheming = true;
+        enableAudioWavelength = true;
+        enableCalendarEvents = true;
       };
-
-      enableSystemMonitoring = true;
-      enableClipboard = true;
-      enableVPN = true;
-      enableDynamicTheming = true;
-      enableAudioWavelength = true;
-      enableCalendarEvents = true;
     };
-  };
 
-  nixos = {
-    systemd.user.services.niri-flake-polkit.enable = false;
+    nixos = {
+      systemd.user.services.niri-flake-polkit.enable = false;
+
+      services.displayManager.dms-greeter = {
+        enable = true;
+        compositor.name = "niri"; # Or "hyprland" or "sway"
+
+        # Sync your user's DankMaterialShell theme with the greeter. You'll probably want this
+        configHome = "/home/oxce5";
+
+        # Save the logs to a file
+        logs = {
+          save = true;
+          path = "/tmp/dms-greeter.log";
+        };
+      };
+    };
   };
 }
