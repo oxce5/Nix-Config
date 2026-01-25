2a6086f087546c5e3bda165bc4b377a54a624413
avg.gamer <avg.gamer@proton.me>
2026-01-25 22:05:39 +0800

    update zellij config

--- modules/programs/zellij/zellij.nix
+++ modules/programs/zellij/zellij.nix
@@ -6,28 +6,11 @@
     ...
   }: let
     zjstatus = inputs.zjstatus.packages.${pkgs.system}.default;
-    tokyoNight = {
-      base00 = "1a1b26";
-      base01 = "16161e";
-      base02 = "2f3549";
-      base03 = "444b6a";
-      base04 = "787c99";
-      base05 = "a9b1d6";
-      base06 = "cbccd1";
-      base07 = "c0caf5";
-      base08 = "f7768e";
-      base09 = "ff9e64";
-      base0A = "e0af68";
-      base0B = "9ece6a";
-      base0C = "73daca";
-      base0D = "7aa2f7";
-      base0E = "bb9af7";
-      base0F = "db4b4b";
-    };
   in {
     programs.zellij = {
       enable = true;
       enableFishIntegration = true;
+      attachExistingSession = true;
       settings = {
         default_shell = "fish";
         styled_underlines = true;
