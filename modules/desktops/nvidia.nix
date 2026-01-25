b0887e794085089e3b95ec7692f49f3aa0822d50
avg.gamer <avg.gamer@proton.me>
2026-01-25 22:07:22 +0800

    switch to stable

--- modules/desktops/nvidia.nix
+++ modules/desktops/nvidia.nix
@@ -17,7 +17,7 @@
         intelBusId = "PCI:0:2:0";
         nvidiaBusId = "PCI:1:0:0";
       };
-      package = config.boot.kernelPackages.nvidiaPackages.production;
+      package = config.boot.kernelPackages.nvidiaPackages.stable;
     };
     services.xserver.videoDrivers = ["nvidia"];
     hardware.graphics = {
